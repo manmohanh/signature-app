3386dd7bbcc16b5b02ca6f36fcdf7c3916f392ef
Manmohan Hansda <hmanmohan12@gmail.com>
2024-08-26 13:00:35 +0530

    completed

--- script.js
+++ script.js
@@ -31,3 +31,36 @@ canvas.addEventListener('mousemove',(e)=>{
   }
 })
 
+canvas.addEventListener('mouseup',() => {
+  isDrawing = false
+})
+
+canvaColor.addEventListener('change',(e) => {
+  ctx.fillStyle = e.target.value;
+  ctx.fillRect(0,0,800,500)
+})
+
+fontPicker.addEventListener('change',(e) => {
+  ctx.lineWidth = e.target.value
+})
+
+clearButton.addEventListener('click',()=>{
+  ctx.clearRect(0,0,canvas.width,canvas.height)
+})
+
+saveButton.addEventListener('click',() => {
+  localStorage.setItem('canvasContents',canvas.toDataURL());
+  let link = document.createElement('a');
+  link.download = 'my-canvas.png'
+  link.href = canvas.toDataURL();
+  link.click();
+})
+
+retrieveButton.addEventListener("click",() => {
+  let savedCanvas = localStorage.getItem('canvasContents')
+  if(savedCanvas){
+    let img = new Image();
+    img.src = savedCanvas;
+    ctx.drawImage(img,0,0)
+  }
+})
